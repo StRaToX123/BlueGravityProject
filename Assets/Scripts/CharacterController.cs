af3d64257e5eccb1b505844753a21d37f3e23809
StRaToX\StRaToX <marinkovic.pavle@gmail.com>
2023-08-15 11:56:14 +0200

    Added the following free asset packages: - Brackeys 2D Mega Pack - Mighty Heroes (Rogue) 2D Fantasy Character Pack - Cainos Pixel Art Top Down
    Loaded in the Cainos top down tileset, and created a character controller script called "CharacterController.cs"
    which moves the rogue character from the Mighty Heroes Pack. Modified the scripts on the stairs asset to properly sort
    the rogue's layers since the character is comprised of multiple sprite renderers.

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CharacterController : MonoBehaviour
+{
+    public float speed;
+
+    private Animator animator;
+    private Rigidbody2D rigidBody2D;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        
+    }
+
+    private void Update()
+    {
+        Vector2 dir = Vector2.zero;
+        if (Input.GetKey(KeyCode.A))
+        {
+            dir.x = -1;
+            // Since we're moving to the left, flip the sprite if it isn't already flipped
+            if (this.gameObject.transform.localScale.x > 0.0f)
+            {
+                Vector3 newLocalScale = this.gameObject.transform.localScale;
+                newLocalScale.x *= -1.0f;
+                this.gameObject.transform.localScale = newLocalScale;
+            }
+            
+            //animator.SetInteger("Direction", 3);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            dir.x = 1;
+            // Since we're moving to the right, flip the sprite if it is already flipped
+            if (this.gameObject.transform.localScale.x < 0.0f)
+            {
+                Vector3 newLocalScale = this.gameObject.transform.localScale;
+                newLocalScale.x *= -1.0f;
+                this.gameObject.transform.localScale = newLocalScale;
+            }
+
+            //animator.SetInteger("Direction", 2);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            dir.y = 1;
+            //animator.SetInteger("Direction", 1);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            dir.y = -1;
+            //animator.SetInteger("Direction", 0);
+        }
+
+        dir.Normalize();
+        animator.SetBool("IsMoving", dir.magnitude > 0);
+
+        rigidBody2D.velocity = speed * dir;
+
+
+    }
+}
